71c3119674ac24fa61656aa0fdeea5c63a1e326a
ttmzmsk <tatemizo.masaki.0810@gmail.com>
2022-07-20 21:31:12 +0900

    ログイン機能追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,11 +1,11 @@
 <?php
- 
+
 namespace App\Http\Controllers\Auth;
- 
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
- 
+
 class LoginController extends Controller
 {
     /*
@@ -18,21 +18,21 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
- 
+
     use AuthenticatesUsers;
- 
+
     /**
-        * Where to redirect users after login.
-        *
-        * @var string
-        */
-    protected $redirectTo = '/tasks';
- 
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
     /**
-        * Create a new controller instance.
-        *
-        * @return void
-        */
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
