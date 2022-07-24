a98a985178785022ac6da84523a260133d55bd46
ttmzmsk <tatemizo.masaki.0810@gmail.com>
2022-07-24 18:05:07 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV2') == 'production' || \App::environment(['develop']))
+        if (env('APP_ENV2') == 'production' || env('APP_ENV2') == 'develop')
         {
         \URL::forceScheme('https');
         }
