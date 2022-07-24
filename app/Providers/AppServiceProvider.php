7e54af160b125f753f31ea8dc652670728e9a706
ttmzmsk <tatemizo.masaki.0810@gmail.com>
2022-07-24 18:02:17 +0900

    chane https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV2') == 'production')
+        if (env('APP_ENV2') == 'production' || \App::environment(['develop']))
         {
         \URL::forceScheme('https');
         }
